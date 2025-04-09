cc3b05c5204cebcce47511f14e519cfaeff06bc7
Navya Valivati <valivati.navya@gmail.com>
2025-04-09 01:30:50 -0500

    displaying top 3 bids in home page

--- auction-platform/src/app/home/home.component.ts
+++ auction-platform/src/app/home/home.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
 import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+import { Auction } from '../auction.model';
+import { AuctionService } from '../auction.service';
+import { TimerComponent } from '../timer/timer.component';
 @Component({
   selector: 'app-home',
-  imports: [NgbCarouselModule, CommonModule],
+  imports: [NgbCarouselModule, 
+            CommonModule, 
+            MatCardModule, 
+            RouterModule, 
+            TimerComponent,
+            MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -13,4 +24,14 @@ export class HomeComponent {
     '/assets/ipad.png',
     '/assets/ipad.png'
   ];
+
+  auctions : Auction[] = [];
+
+  constructor(private auctionService: AuctionService) {}
+
+  ngOnInit(): void {
+    this.auctionService.getAllAuctions().subscribe((data) => {
+      this.auctions = data.slice(0,3);
+    });
+  }
 }
